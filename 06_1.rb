d4dc35bdbe54236886844afea2be3374e7e6bda0
schlecker <simon.hoelscher@posteo.de>
2018-12-06 15:53:21 +0100

    solutions getting longer and longer... oh my

---
+++ 06_1.rb
@@ -0,0 +1,93 @@
+### part 1 ###
+
+def md a,b 
+  (a[0]-b[0]).abs + (a[1]-b[1]).abs
+end
+
+coords = Array.new
+
+lines = 0
+
+# Read file
+File.open("inputs/06").each do |line|
+  coords << [line.split(', ')[0].to_i,line.split(', ')[1].strip.to_i]
+  lines+=1
+end
+
+xsize = coords.map{|c| c[0]}.max + 1
+ysize = coords.map{|c| c[1]}.max + 1
+
+field = Array.new(ysize) {Array.new(xsize, '.')}
+
+# For the whole field... 
+for i in 0..ysize-1
+  for j in 0..xsize-1
+    dists = Array.new
+    # ... calculate distance to each coordinate
+    for k in 0..lines-1
+      dists[k] = md([j,i], coords[k])
+    end
+    # Contested minimum distances don't count
+    if dists.count(dists.min) > 1
+      field[i][j] = '.'
+    else
+      field[i][j] = dists.index(dists.min)+1
+    end
+  end
+end
+
+# Find largest non-infinite field
+
+legit_fields = (1..50).to_a
+
+# Check left and right border
+
+for i in 0..ysize-1
+  for k in 0..lines-1
+    if field[i][0] == k
+      legit_fields.delete(k)
+    end
+  end
+  for k in 0..lines-1
+    if field[i][xsize-1] == k
+      legit_fields.delete(k)
+    end
+  end
+end
+
+# Check top and bottom border
+
+for i in 0..xsize-1
+  for k in 0..lines-1
+    if field[0][i] == k
+      legit_fields.delete(k)
+    end
+  end
+  for k in 0..lines-1
+    if field[ysize-1][i] == k
+      legit_fields.delete(k)
+    end
+  end
+end
+
+# "Flatten" the two dimensional field 
+wholefield = Array.new
+field.each do |y|
+  wholefield += y
+end
+
+# Only take the non-infinite fields and count the occurrences
+count = Hash.new 0
+wholefield.select { |num| legit_fields.include?(num) }.each do |coord|
+  count[coord]+=1
+end
+
+# Find maximum
+
+puts count.max_by {|key,value| value}
+
+# Print field
+#for i in 0..ysize-1
+#  puts field[i].join(' ')
+#end
+
---
+++ 06_2.rb
@@ -0,0 +1,51 @@
+### part 2 ###
+
+def md a,b 
+  (a[0]-b[0]).abs + (a[1]-b[1]).abs
+end
+
+coords = Array.new
+
+lines = 0
+
+# Read file
+File.open("inputs/06").each do |line|
+  coords << [line.split(', ')[0].to_i,line.split(', ')[1].strip.to_i]
+  lines+=1
+end
+
+xsize = coords.map{|c| c[0]}.max + 1
+ysize = coords.map{|c| c[1]}.max + 1
+
+field = Array.new(ysize) {Array.new(xsize, '.')}
+
+# For the whole field... 
+for i in 0..ysize-1
+  for j in 0..xsize-1
+    dist = 0
+    # ... calculate sum of distances to each coordinate
+    for k in 0..lines-1
+      #puts "adding " + md([j,i], coords[k]).to_s
+      dist += md([j,i], coords[k])
+    end
+    # Contested minimum distances don't count
+    if dist < 10000
+      field[i][j] = '#'
+    else
+      field[i][j] = '.'
+    end
+  end
+end
+
+# "Flatten" the two dimensional field 
+wholefield = Array.new
+field.each do |y|
+  wholefield += y
+end
+
+puts wholefield.count("#")
+
+# Print field
+for i in 0..ysize-1
+  puts field[i].join(' ')
+end

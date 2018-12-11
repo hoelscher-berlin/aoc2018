02b88efae77765e15e16efd7ce42c8dd91abc89e
schlecker <simon.hoelscher@posteo.de>
2018-12-11 14:18:25 +0100

    a bit more efficient, still too slow

--- 11.rb
+++ 11.rb
@@ -1,6 +1,7 @@
 grid_snr = 8141
 
 grid = Array.new(301) {Array.new(301)}
+lastpower = Array.new(301) {Array.new(301)}
 
 for x in 1..300
   for y in 1..300
@@ -17,30 +18,29 @@ for x in 1..300
     end
     powerlevel -= 5
     grid[x][y] = powerlevel
+    lastpower[x][y] = 0
   end
 end
 
 max= {:coords => [0,0], :value => 0, :size => 0}
 for size in 1..300
-  puts size
-  for x in 1..300-size
-    for y in 1..300-size
-      sum = 0
-      for i in 0..size-1
-        for j in 0..size-1
-          sum += grid[x+i][y+j]
-        end
+  for x in 1..300-size+1
+    for y in 1..300-size+1
+      sum = lastpower[x][y]
+      for i in x..x+size-1
+        sum += grid[i][y+size-1]
       end
-      #puts sum
+      for i in y..y+size-1
+        sum += grid[x+size-1][i]
+      end
+      lastpower[x][y] = sum 
       if sum > max[:value]
         max[:coords] = [x,y]
         max[:value] = sum
         max[:size] = size
-        print max
       end
     end
   end
 end
 
-#print grid
 print max
